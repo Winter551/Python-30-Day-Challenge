6cb1fe7b93f33e360b27aa140dd7cda3129d966b
fificodes <sophielin507@gmail.com>
2023-09-05 17:52:46 -0400

    day two code

---
+++ sophie's code <3/day_two.py
@@ -0,0 +1,7 @@
+# program that calculates user age
+
+current_year = 2023
+birth = int(input("what year were you born in? "))
+
+age = current_year - birth
+print("wow, you are " + str(age) + " years old already!")
