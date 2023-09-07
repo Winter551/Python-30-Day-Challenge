c5c665a9bd1256fb4ea29cc889430fa81877ff4b
Winter551 <143036713+Winter551@users.noreply.github.com>
2023-09-07 10:40:17 -0500

    More comments

--- Sam Code/day3.py
+++ Sam Code/day3.py
@@ -1,11 +1,14 @@
+#Importing "random" library
 import random
 
-
+#Ask user for user number
 print("This program will randomly select a number from 1-10 and multiply it by your chosen number")
 user_num = int(input("Please select your number: "))
 
+#Multiplying the random number by the user number
 rand_num = random.randint(1,10)
 sum_num = (user_num * rand_num)
 
+#Print math stuff
 print(f"The random number was: [{rand_num}]")
 print(f"The sum of [{rand_num}] and [{user_num}] is [{sum_num}]")
